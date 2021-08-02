76e327154aed2c23f8186984211583f8e41d6983
Solomon-Ay <77963733+Solomon-Ay@users.noreply.github.com>
2021-08-02 19:24:36 +0100

    Add files via upload

---
+++ stage_0_Solomon (1).jl
@@ -0,0 +1,17 @@
+Name = "Solomon Olorundare"
+
+Email =  "olorundaresolomonay@gmail.com"
+
+Slack_Username = "Solomon"
+
+Biostack = "Genomics"
+
+
+
+print(Name, "\n")
+
+print(Email, "\n")
+
+print(Slack_Username, "\n")
+
+print(Biostack)
