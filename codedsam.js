52f235ed23f22676ec27234f48fcd82e0140ddc9
Samuel Anozie <72805827+samcoded@users.noreply.github.com>
2021-08-02 10:22:34 +0100

    Update codedsam.js

--- codedsam.js
+++ codedsam.js
@@ -1,1 +1,8 @@
-console.log("Samuel Anozie");
+const name = "Samuel Anozie";
+const email = "samuelanozie10@yahoo.com";
+const slack = "@CodedSAM";
+
+
+console.log(name);
+console.log(email);
+console.log(slack);
