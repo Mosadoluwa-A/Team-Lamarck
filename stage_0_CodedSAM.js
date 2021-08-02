7029aeeddd187e7181fc02430f6128435e3a7cf6
Samuel Anozie <72805827+samcoded@users.noreply.github.com>
2021-08-02 18:30:22 +0100

    Update and rename codedsam.js to stage_0_CodedSAM.js

--- stage_0_CodedSAM.js
+++ stage_0_CodedSAM.js
@@ -1,8 +1,9 @@
 const name = "Samuel Anozie";
 const email = "samuelanozie10@yahoo.com";
 const slack = "@CodedSAM";
-
+const biostack = "Software Development";
 
 console.log(name);
 console.log(email);
 console.log(slack);
+console.log(biostack);
