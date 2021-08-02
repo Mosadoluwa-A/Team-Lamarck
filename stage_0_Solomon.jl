10f758fea0bd575409b9ffdcacc92ed03baae3c4
Solomon-Ay <77963733+Solomon-Ay@users.noreply.github.com>
2021-08-02 20:40:20 +0100

    Update stage_0_Solomon.jl

--- stage_0_Solomon.jl
+++ stage_0_Solomon.jl
@@ -1,17 +1,9 @@
 Name = "Solomon Olorundare"
-
 Email =  "olorundaresolomonay@gmail.com"
-
-Slack_Username = "Solomon"
-
+Slack_Username = "@Solomon"
 Biostack = "Genomics"
 
-
-
 print(Name, "\n")
-
 print(Email, "\n")
-
 print(Slack_Username, "\n")
-
 print(Biostack)
