9e26d177a96dedee2b5ab7885f7767bf83368a0f
Mostafa A. Radian <mostafa.batesta@gmail.com>
2021-08-04 11:21:39 +0200

    Update stage_0_Mostafa.cpp

--- stage_0_Mostafa.cpp
+++ stage_0_Mostafa.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 using namespace std;
+int CalulateHamming (string twitter, string slack)
+{
+    int counter = 0;
+    for(int i = 0,j= 0; static_cast<void>(i<twitter.length()),j<slack.length();i++,j++)
+    {
+        if(twitter[i]!=slack[j])
+            counter++;
+    }
+    return  counter;
+}
 int main() {
-    string name = "Mostafa", email = "mostafa.batesta@gmail.com", username = "@Mostafa", biostack = "Data Science";
+    string name = "Mostafa", email = "mostafa.batesta@gmail.com", username = "@Mostafa", biostack = "Data Science", twitterhandle = "@Mostafa5739201081", slackhandle = "@Mostafa.slack.com";
+    int hammingdis = CalulateHamming(twitterhandle, slackhandle);
     
-    cout << name<<"  "<< email<<"  "<< username <<"  "<< biostack;
+    cout << name<<"  "<< email<<"  "<< username <<"  "<< biostack<<"  "<< twitterhandle<<"  "<< hammingdis<< endl;
     return 0;
 }
 
