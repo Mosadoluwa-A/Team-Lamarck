48db673a79b05d686131d966e706dc68d2bb1342
Samuel Anozie <samuelanozie10@yahoo.com>
2021-08-04 20:26:44 +0100

    added twitter handle

--- stage_0_CodedSAM.js
+++ stage_0_CodedSAM.js
@@ -1,9 +1,14 @@
 const name = "Samuel Anozie";
 const email = "samuelanozie10@yahoo.com";
 const slack = "@CodedSAM";
-const biostack = "Genomic";
+const biostack = "Software Development";
+const twitter = "@CodedSAM_";
+
+const hamming = "3";
 
 console.log(name);
 console.log(email);
 console.log(slack);
 console.log(biostack);
+console.log(twitter);
+console.log(hamming);
