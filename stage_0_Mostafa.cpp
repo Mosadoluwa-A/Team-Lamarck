e6a3cdd7ae85055fd4ebbc1d6b80741e49432225
Mostafa A. Radian <mostafa.batesta@gmail.com>
2021-08-04 10:14:06 +0200

    added task that is made using C++

---
+++ stage_0_Mostafa.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string name = "Mostafa", email = "mostafa.batesta@gmail.com", username = "@Mostafa", biostack = "Data Science";
+    
+    cout << name<<"  "<< email<<"  "<< username <<"  "<< biostack;
+    return 0;
+}
+
