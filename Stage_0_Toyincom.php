cbb83ba69204b13fbff4f670d73b268d25e88ff3
Mosadoluwa-A <magbonyin@gmail.com>
2021-08-07 15:24:19 +0100

    Bug Fixes

--- Stage_0_Toyincom.php
+++ Stage_0_Toyincom.php
@@ -1,60 +1,30 @@
 <?php
-   $name="Oluwatoyin Akamo";
-   $email="Oduntee@email.com";
-   $slack="@Toyincom";
-   $biostack="software development";
-   $twitter="@Tee";
+	
+	$name="Oluwatoyin Akamo";
+   	$email="Oduntee@email.com";
+   	$slack="@Toyincom";
+   	$biostack="software development";
+   	$twitter="@Teetoyin";
 
-// PHP program to find hamming distance b/w 
+	function hammingDist($str1, $str2) {
+		$i = 0; 
+		$count = 0; 
 
-// two string
+		while(isset($str1[$i])) {
+  			if($str1[$i] != $str2[$i]){
+  				
+  				$count++;
+  			}
+  			$i++;
+		} 
+		echo $count;
+	}
 
- 
+	echo $name."\r\n";
+	echo $email. "\r\n";
+	echo $slack. "\r\n";
+	echo $biostack. "\r\n";
+	echo $twitter. " \r\n";
 
-// function to calculate
-
-// Hamming distance
-
-function hammingDist($str1, $str2) 
-
-{
-
-    $i = 0; $count = 0; 
-
-    while (isset($str1[$i]) != '') 
-
-    { 
-
-        if ($str1[$i] != $str2[$i]) 
-
-            $count++; 
-
-        $i++; 
-
-    } 
-
-    return $count; 
-
-}
-
- 
-
-    // Driver Code 
-
-    $str1 = "team"; 
-
-
-    $str2 = "teen";
-
-
-echo $name."\r\n";
-echo $email. "\r\n";
-echo $slack. "\r\n";
-echo $biostack. "\r\n";
-echo $twitter. " \r\n";
- 
-// function call 
-
-    echo hammingDist ($str1, $str2);
-
+	hammingDist($slack,$twitter)
 ?>
