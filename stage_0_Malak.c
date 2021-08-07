d4fc65b0188676b07dfbfc8d7c2351e1fb241259
malaksoliman <71707842+malaksoliman@users.noreply.github.com>
2021-08-07 19:59:32 +0200

    Update stage_0_Malak.c

--- stage_0_Malak.c
+++ stage_0_Malak.c
@@ -1,40 +1,34 @@
 #include <stdio.h>
 #include <string.h>
+//creating hamming distance function
+int hammingDistance(char string1[], char string2[])
+{
+    int i = 0, count = 0;
+    while(string1[i]!='\0')
+    {
+        if (string1[i] != string2[i])
+            count++;
+        i++;
+    }
+    return count;
+}
+int main(void) {
 
-int main() {
 char Name[]="Malak Mohamed Soliman ";
 char Email[]="M.abdelmonsef@nu.edu.eg";
 char Slack_userName[]="@Malak";
 char biostack[]= "Genomics";
 char Twitter_Handel[]="@Malak";
-//create Hamming distance Function
-int i;
-int count;
-char str1[30];
-char str2[30];
-int hammingDist()
-{
-    i = 0;
-    count = 0;
- 
-    while(i < strlen(str1))
-    {
-        if(str1[i] != str2[i]){
-            count += 1;
-        i += 1;
-    return count;
-            
-        }
-    }
-}
-
+//Calling the function
+int hammingDist();
 printf("Name:%s\n",Name);
 printf("E-mail:%s\n",Email);
 printf("Slack_userName:%s\n",Slack_userName);
 printf("biostack:%s\n",biostack);
 printf("Twitter_Handel:%s\n",Twitter_Handel);
-//function call
-printf("hammingDistance:%d",hammingDist(Slack_userName, Twitter_Handel));
-    
+//function printing
+printf("hammingDistance:%d",hammingDistance(&Slack_userName,
+&Twitter_Handel));
+
     return 0;
 }
